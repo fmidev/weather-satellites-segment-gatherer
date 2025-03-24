32184cfebcc3ca783e154ed288d21807ec0093ec
Panu Lahtinen <panu.lahtinen@fmi.fi>
2025-03-24 10:53:54 +0200

    Add SIGTERM handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/bash
 
+_term() {
+  echo "Entrypoint caught SIGTERM signal"
+  kill -TERM "$child" 2>/dev/null
+  echo "Waiting for child process to exit"
+  wait "$child"
+}
+
+trap _term SIGTERM
+
 source /opt/conda/.bashrc
 source /config/env-variables
 
 micromamba activate
 if [ -e /config/segment_gatherer.yaml ]; then
-    /opt/conda/bin/segment_gatherer.py -v -c /config/segment_gatherer.yaml
+    /opt/conda/bin/segment_gatherer.py -v -c /config/segment_gatherer.yaml &
+    child=$!
 else
-    /opt/conda/bin/segment_gatherer.py -v -c /config/segment_gatherer.conf -C default
+    /opt/conda/bin/segment_gatherer.py -v -c /config/segment_gatherer.conf -C default &
+    child=$!
 fi
+
+wait "$child"
